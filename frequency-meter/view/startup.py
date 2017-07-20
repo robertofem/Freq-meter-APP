3d5e6247aa4384b38a851e8296be212542f86b7c
jlrandulfe <jlrandulfe@gmail.com>
2017-07-20 16:50:18 +0200

    Add check buttons for establishing enabled log screen levels
    One they are checked/unchecked, the future log messages are printed
    accordingly with the state of the buttons.
    
    Add as well the PyVisa library to the package requirements.

--- frequency-meter/view/startup.py
+++ frequency-meter/view/startup.py
@@ -24,18 +24,27 @@ class AppLogHandler(logging.Handler):
         formatter = logging.Formatter(" %(asctime)s.%(msecs)03d %(levelname)8s:"
                                       " %(message)s", "%H:%M:%S")
         self.setFormatter(formatter)
+        # Log messages colours.
         self.levelcolours = {
             logging.DEBUG: 'black',
             logging.INFO: 'blue',
             logging.WARN: 'orange',
             logging.ERROR: 'red',
         }
+        # Paths to the log icons.
         parent_path = os.path.dirname(__file__)
         self.logsymbols = {
             logging.DEBUG: "{}/icons/debug.png".format(parent_path),
             logging.INFO: "{}/icons//info.png".format(parent_path),
             logging.WARN: "{}/icons//warning.png".format(parent_path),
             logging.ERROR: "{}/icons//error.png".format(parent_path),
+        }
+        # The True levels are the ones that are printed on the log.
+        self.enabled = {
+            logging.DEBUG: False,
+            logging.INFO: True,
+            logging.WARN: True,
+            logging.ERROR: True,
         }
 
     def emit(self, record):
@@ -44,6 +53,9 @@ class AppLogHandler(logging.Handler):
         The received log message will be printed on the specified
         widget, tipically a TextBox.
         """
+        # Only print on the log the enabled log levels.
+        if not self.enabled[record.levelno]:
+            return
         new_log = self.format(record)
         self.widget.insertHtml('<img src={img} height="14" width="14"/>'
                                '<font color="{colour}">{log_msg}</font><br />'
@@ -51,6 +63,7 @@ class AppLogHandler(logging.Handler):
                                        colour=self.levelcolours[record.levelno],
                                        log_msg=new_log))
         self.widget.moveCursor(QtGui.QTextCursor.End)
+        return
 
 
 class MainWindow(QtGui.QMainWindow, interface.Ui_MainWindow):
@@ -60,14 +73,28 @@ class MainWindow(QtGui.QMainWindow, interface.Ui_MainWindow):
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
         self.setupUi(self)
+        # Test buttons
         self.DebugButton.clicked.connect(self.debug)
         self.ErrorButton.clicked.connect(self.error)
         self.WarnButton.clicked.connect(self.warn)
+        # Log console level selection buttons
+        self.DebugCheck.clicked.connect(self.update_logger_level)
+        self.InfoCheck.clicked.connect(self.update_logger_level)
+        self.WarnCheck.clicked.connect(self.update_logger_level)
+        self.ErrorCheck.clicked.connect(self.update_logger_level)
         # Configure the logger, assigning an instance of AppLogHandler.
-        log_handler = AppLogHandler(self.LoggerBrowser)
+        self.log_handler = AppLogHandler(self.LoggerBrowser)
         # log_handler.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
-        logger.addHandler(log_handler)
+        logger.addHandler(self.log_handler)
         logger.info("Initialized the Frequency-Metter Application")
+
+    def update_logger_level(self):
+        """Evaluate the checkboxes states and update logger level."""
+        self.log_handler.enabled[logging.DEBUG] = self.DebugCheck.isChecked()
+        self.log_handler.enabled[logging.INFO] = self.InfoCheck.isChecked()
+        self.log_handler.enabled[logging.WARN] = self.WarnCheck.isChecked()
+        self.log_handler.enabled[logging.ERROR] = self.ErrorCheck.isChecked()
+        return
 
     def debug(self):
         logger.debug("Pressed debug button")
